92c185090b1c956b65bbeb8ce3048b3690b78d32
hazarkir <kirhazar@gmail.com>
2023-12-07 23:04:40 +0300

    first commit

---
+++ bugünNeÖğrendim.js
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>1 Videoda Javascript</title>
+    <style>
+
+        .header {
+            text-align: center;
+            font-family: sans-serif;
+            font-size: 18px;
+            background: red;
+        }
+
+        .container {
+            text-align: center;
+            font-size: 30px;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="header">
+
+        <h1>1 Videoda Javascript Nedir? Ne işe Yarar? Neler Öğrendim?</h1>
+
+    </div>
+
+    <hr>
+    <hr>
+
+    <div class="container">
+       <ul>
+        <ol>Değişkenler ve Veri Tipleri</ol>
+        <ol>Arrayler ve Fonksiyonları</ol>
+        <ol> Visual Studio Kurulumu </ol>
+        <ol> Eklentileri kur </ol>
+        <ol> Javascripti dahil etme </ol>
+        <ol>Tarayıcı consolu kullanımı </ol>
+        <ol>Derse giriş - Değişken oluşturma </ol>
+        <ol>Array (Dizi) </ol>
+        <ol>For döngüsü </ol>
+        <ol>While döngüsü </ol>
+        <ol>forEach fonksiyonu </ol>
+        <ol>If Else (Koşullar) </ol>
+        <ol>Switch Case </ol>
+        <ol>Fonksiyonlar </ol>
+        <ol>Obje oluşturma (Object based programming) </ol>
+        <ol>Constructor pattern </ol>
+        <ol>Eventler </ol>
+        <ol>Form İşlemleri </ol>
+       </ul>
+       
+
+    </div>
+    <script>    
+       
+
+    </script>
+</body>
+</html>
